d7fbd18c48b4c2118975dfb330b4410cedb714f9
Jeries <Jeriesd1998@gmail.com>
2018-12-30 18:20:11 -0500

    added php directory, where the scripts will be added. Found the login form and replaced the original action with my intermediate script to steal the information.

---
+++ php/login.php
@@ -0,0 +1,11 @@
+<?php
+  /* This is the phishing script, replaced the action on the login form with
+    this script, and the original action was to go to this
+    "https://www.facebook.com/login/device-based/regular/login/?login_attempt=1&amp;lwv=110",
+  */
+
+
+
+
+
+ ?>
