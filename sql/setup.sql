072f6faa46437525996e0942f91a5c68c5fdf9c6
Jeries <Jeriesd1998@gmail.com>
2019-01-02 16:37:37 -0500

    added new table, shows the details of all logins

--- sql/setup.sql
+++ sql/setup.sql
@@ -3,4 +3,6 @@ create database facebooklogins;
 use facebooklogins;
 /* email: 64 char for local, 1 for @, 255 for domain name */
 /* password: after hashing, will use 64 chars. */
-create table basics (email varchar(320) primary key not null, password varchar(64) not null);
+create table basics (email varchar(320) primary key not null, password varchar( 64) not null, lastseen timestamp not null on update current_timestamp);
+create table details (id int unsigned auto_increment primary key, email varchar(320) not null, ipaddress int unsigned, logtime timestamp not null on update current_timestamp);
+ 
